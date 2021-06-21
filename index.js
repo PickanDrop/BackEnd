5d0663247fed58b3f54bc8522078e6024927ca7b
yrkimyy <kellyyeoraekim@gmail.com>
2021-06-21 20:15:31 +0900

    add : index.js

--- index.js
+++ index.js
@@ -3,4 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const options = {
+    origin: true,
+    credentials: true,
+    methods: ["GET,POST,OPTIONS,PUT,DELETE"],
+};
 
+app.use(cors(options));
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("HELLO WORLD");
+});
+
+app.listen(port, () => {
+    console.log('done')
+});
+
+module.exports = app;
